1b77df163b2bc8b695fb96098843784bffdf130e
Nguyen Thanh Son <son_nguyen_thanh@manulife.com>
2020-07-21 11:32:33 +0700

    - Fix the max heap while building

--- Angular/ng4-complete-guide/src/app/app.component.ts
+++ Angular/ng4-complete-guide/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng4-complete-guide';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string): void {
+    this.loadedFeature = feature;
+  }
+
 }

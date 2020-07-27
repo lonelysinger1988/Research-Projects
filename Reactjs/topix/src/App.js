9d487496a9c5c26c8d34facd169664779481aab2
Nguyen Thanh Son <son_nguyen_thanh@manulife.com>
2020-07-27 15:17:48 +0700

    Use history.push (withRouter)

---
+++ Reactjs/topix/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Route, Link } from 'react-router-dom';
+
+import './App.css';
+
+const HomePage = props => {
+  return (
+    <div>
+      <button onClick={ () => props.history.push('/topics') }>Topics</button>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE : { props.match.params.topicId }</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
+    </div>
+  );
+}
+
+export default App;
